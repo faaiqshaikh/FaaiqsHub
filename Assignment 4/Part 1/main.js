322304db38ba2686b3b751b7fc417cef5658cc80
faaiqshaikh <mohammedfaiq.shaikh@dcmail.ca>
2024-08-01 16:00:00 -0400

    replaced the name and numerical values
    updated first if condition to update the entered name and added calculations for conversions. Then updated the string with new values and displayed string.

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -16,20 +16,6 @@ const storyText = "It was 94 fahrenheit outside, so  went for a walk. When they 
 randomize.addEventListener('click', result);
 
 function result() {
-
-  if(customName.value !== '') {
-    const name = customName.value;
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
     let newStory = storyText;
     let xItem = randomValueFromArray(insertX);
     let yItem = randomValueFromArray(insertY);
@@ -37,4 +23,24 @@ function result() {
     newStory = newStory.replace(/:insertx:/g, xItem);
     newStory = newStory.replace(/:inserty:/g, yItem);
     newStory = newStory.replace(/:insertz:/g, zItem);
+
+    if(customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replace(/Bob/g, name);
+    }
+
+    if(document.getElementById("uk").checked) {
+        let stone = 300/14;
+        let centigrade = ((94-32)*5)/9;
+        const weight = Math.round(stone);
+        newStory = newStory.replace(/300/g, weight)
+        newStory = newStory.replace(/pounds/g, "stones");
+
+        const temperature =  Math.round(centigrade);
+        newStory = newStory.replace(/94/g, temperature);
+        newStory = newStory.replace(/fahrenheit/g, "centigrade");       
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
