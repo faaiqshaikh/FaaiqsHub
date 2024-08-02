4a64bcbd7c3e1a3ad4f7cbf85bfe68058d760956
faaiqshaikh <mohammedfaiq.shaikh@dcmail.ca>
2024-08-01 20:12:23 -0400

    Created Array for Images
    filenames array for all images in the folder and  alt_filenames array for alternative text.

--- Assignment 4/Part 2/gallery-start/main.js
+++ Assignment 4/Part 2/gallery-start/main.js
@@ -5,8 +5,9 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
+let filenames = ['images/pic1.jpg','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'];
+/* Declaring, the alternative text for each image file */
+let alt_filenames = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Looping through images */
 
